048d4f44fa72d3e3258822984526089679424334
yushi777 <ucy77777@gmail.com>
2022-12-16 10:20:12 +0900

    コントローラーにリクエストを送信する

---
+++ app/javascript/memo.js
@@ -0,0 +1,15 @@
+function post (){
+  //リクエストを送信する処理
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
+};
+
+window.addEventListener('load', post);
