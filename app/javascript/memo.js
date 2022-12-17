67ba29ec2d989342f3b17930c4ffb9a3b7ee87c8
yushi777 <ucy77777@gmail.com>
2022-12-17 23:10:59 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,5 +1,18 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+  <div class="post">
+  <div class="post-date">
+  投稿日時：${item.created_at}
+  </div>
+  <div class="post-content">
+  ${item.content}
+  </div>
+  </div>`;
+  return html;
+};
+
 function post (){
-  //リクエストを送信する処理
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
@@ -9,6 +22,16 @@ function post (){
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
 };
 
